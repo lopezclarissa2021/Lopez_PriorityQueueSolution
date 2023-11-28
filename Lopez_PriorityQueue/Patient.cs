f4d26a004da6d83cdbe9ce15f20b40946c595b1a
Clarissa Lopez <102737214+lopezclarissa2021@users.noreply.github.com>
2023-11-28 05:28:59 -0800

    Add project files.

---
+++ Lopez_PriorityQueue/Patient.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lopez_PriorityQueue
+{
+    internal class Patient
+    {
+        string LastName;
+        string FirstName;
+        DateTime Birthdate;
+        int Priority;
+
+        public Patient(string lastName, string firstName, DateTime birthdate, int priority)
+        {
+            LastName = lastName;
+            FirstName = firstName;
+            Birthdate = birthdate;
+            Priority = priority;
+
+            SetPriority();
+        }
+
+        public override string ToString()
+        {
+            return $"{LastName}, {FirstName}, {Birthdate}, {Priority}";
+        }
+
+        public void SetPriority()
+        {
+            if (Age < 21 || Age > 65)
+            {
+                this.Priority = 1;
+            }
+        }
+        public int PriorityLevel { get => this.Priority; }
+        public int Age
+        {
+            get
+            {
+                int age = 0;
+                age = DateTime.Now.Year - Birthdate.Year;
+                if (DateTime.Now.DayOfYear < Birthdate.DayOfYear)
+                    age = age - 1;
+
+                return age;
+            }
+        }
+    }
+}
---
+++ Lopez_PriorityQueue/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace Lopez_PriorityQueue
+{
+    internal class Program
+    {
+        static bool DoExit = false;
+        static PriorityQueue<Patient, int> ERQueue = new PriorityQueue<Patient, int>();
+        static void Main(string[] args)
+        {
+            using (StreamReader? reader = new StreamReader(@$"{Directory.GetCurrentDirectory()}\Patients-1.csv"))
+            {
+                string contents = reader.ReadToEnd();
+                string[] lines = contents.Split(Environment.NewLine);
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    if (i == 0)
+                        continue;
+
+                    string line = lines[i];
+
+                    if (string.IsNullOrEmpty(line.Trim()))
+                        continue;
+
+                    string[] values = line.Split(',');
+
+                    string firstName = values[0];
+                    string lastName = values[1];
+                    DateTime birthDate = DateTime.Parse(values[2]);
+                    int priority = int.Parse(values[3]);
+
+                    Patient patient = new Patient(firstName, lastName, birthDate, priority);
+
+                    ERQueue.Enqueue(patient, patient.PriorityLevel);
+                }
+            }
+
+            while (!DoExit)
+            {
+
+                Console.WriteLine($"A. Add Patient {Environment.NewLine}P.Process Current Patient {Environment.NewLine}L. List All in Queue {Environment.NewLine}Q. Exit");
+                string result = Console.ReadLine();
+                switch (result)
+                {
+                    case "A":
+                        Console.WriteLine("What is the patients first name to add?");
+                        string firstNameToAdd = Console.ReadLine().Trim();
+                        Console.WriteLine("What is the patients last name to add?");
+                        string lasttNameToAdd = Console.ReadLine().Trim();
+                        Console.WriteLine("What is the patients birthdate?");
+                        DateTime birthdateToAdd = DateTime.Parse(Console.ReadLine().Trim());
+                        Console.WriteLine("What is the priority level?");
+                        int priorityLevel = int.Parse(Console.ReadLine().Trim());
+
+                        Patient patient = new Patient(lasttNameToAdd, firstNameToAdd, birthdateToAdd, priorityLevel);
+                        ERQueue.Enqueue(patient, patient.PriorityLevel);
+                        break;
+                    case "P":
+                        // remove current patient
+                        if (ERQueue.Count > 0)
+                        {
+                            ERQueue.Dequeue();
+                            Console.WriteLine("removed patient");
+                        }
+                        else
+                        {
+                            Console.WriteLine("No more patients in Queue.");
+                        }
+
+                        break;
+                    case "L":
+                        // list all in queue
+                        List<Patient> patients = ERQueue.UnorderedItems.Select(p => p.Element).OrderBy(p => p.PriorityLevel).ToList();
+                        foreach (Patient p in patients)
+                        {
+                            Console.WriteLine($"{p}");
+                        }
+                        break;
+                    case "Q":
+                        DoExit = true;
+                        break;
+                }
+            }
+        }
+    }
+}
